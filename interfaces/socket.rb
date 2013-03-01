2e0b28a62f4b2d19a11adc95d96d2361034b77d9
Julien Ammous <schmurfy@gmail.com>
2013-03-01 16:11:14 +0100

    namespace classes

--- interfaces/socket.rb
+++ interfaces/socket.rb
@@ -1,13 +1,14 @@
 
-class SocketServer < Collectd
-  def initialize(server, port, host, interval)
-    @sock = UDPSocket.new(Socket::AF_INET)
-    @sock.connect(server, port)
-    super
-  end
-  
-  def flush
-    @sock.send(@pkt.to_s, 0) rescue SystemCallError
+module Collectd
+  class SocketServer < Base
+    def initialize(server, port, host, interval)
+      @sock = UDPSocket.new(Socket::AF_INET)
+      @sock.connect(server, port)
+      super
+    end
+    
+    def flush
+      @sock.send(@pkt.to_s, 0) rescue SystemCallError
+    end
   end
 end
-
