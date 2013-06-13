8e5db74bb15908cff28655204d92eda495be9a19
Julien Ammous <schmurfy@gmail.com>
2013-06-13 12:26:11 +0200

    bind stat socket on localhost by default

--- interfaces/em.rb
+++ interfaces/em.rb
@@ -3,9 +3,9 @@ require 'eventmachine'
 
 module CollectdProtocol
   class EMServer < Base
-    def initialize(server, port = 25826, host = nil, interval = 10, bind_address = "0.0.0.0")
+    def initialize(server, port = 25826, host = nil, interval = 10, bind_address = nil)
 
-      @sock = EM::open_datagram_socket(bind_address, nil)
+      @sock = EM::open_datagram_socket(bind_address || '127.0.0.1', nil)
       super(server, port, host, interval)
     end
     
